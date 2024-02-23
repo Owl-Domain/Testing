25cc4b04387930d12b1f7b8d51ab5a8f22921a65
Nightowl <79215414+nightowl286@users.noreply.github.com>
2024-02-23 20:21:22 +0000

    + Added unit tests for the MSTest specific implementations. * Small capitalisation fix in the MSTest specific project file. * Small capitalisation fix in the base Testing project file.

---
+++ src/Testing.MSTest.Tests/FailAssertTests.cs
@@ -0,0 +1,38 @@
+﻿namespace Testing.MSTest.Tests;
+
+[TestClass]
+public class FailAssertTests
+{
+   #region Fields
+   private readonly FailAssert _sut = new();
+   #endregion
+
+   #region Tests
+   [TestMethod]
+   public void Fail_WithMessage_ThrowsAssertFailedException()
+   {
+      // Arrange
+      string message = "message";
+
+      // Act
+      void Act() => _sut.Fail(message);
+
+      // Assert
+      MSAssert.ThrowsException<AssertFailedException>(Act);
+   }
+
+   [TestMethod]
+   public void Fail_WithFormat_ThrowsAssertFailedException()
+   {
+      // Arrange
+      string format = "{0} format {1}";
+      object?[] args = [1, 2];
+
+      // Act
+      void Act() => _sut.Fail(format, args);
+
+      // Assert
+      MSAssert.ThrowsException<AssertFailedException>(Act);
+   }
+   #endregion
+}
---
+++ src/Testing.MSTest.Tests/Global.cs
@@ -0,0 +1,9 @@
+﻿global using Microsoft.VisualStudio.TestTools.UnitTesting;
+global using OwlDomain.Testing.MSTest;
+global using MSAssert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+#if DEBUG
+[assembly: Parallelize(Scope = ExecutionScope.ClassLevel, Workers = 1)]
+#else
+[assembly: Parallelize(Scope = ExecutionScope.MethodLevel, Workers = 0)]
+#endif
